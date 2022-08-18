7639f6347a8259bed05785f40d5243b8795f5ec3
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-08-18 08:27:15 -0700

    Initial Commit

---
+++ AppCuenta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCuenta
+//
+//  Created by Alumno on 8/18/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
