77f728b4b1b94ef2998509f7f508e524e37dd622
Lizbeth Baez <lizbaez.140799@gmail.com>
2022-08-18 09:46:57 -0700

    Cuenta y propina

--- AppCuenta/ViewController.swift
+++ AppCuenta/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblPropina: UISlider!
+    @IBOutlet weak var lblPropinaValue: UILabel!
+    @IBOutlet weak var lblPropinaText: UITextField!
+    @IBOutlet weak var lblPropinaTotal: UILabel!
+    @IBOutlet weak var lblTotalConPropina: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sliderPropina(_ sender: Any) {
+        lblPropinaValue.text = "\(Int(lblPropina.value))%"
+        
+        let cuenta: String = lblPropinaText.text!
+        let cuentaInt = Int(cuenta) ?? 0
 
-
+        let propina = Int(lblPropina.value)
+        let propinacal = ((cuentaInt * propina)/100)
+        
+        lblPropinaTotal.text = "$\(propinacal)"
+        lblTotalConPropina.text = "$\(propinacal + cuentaInt)"
+    }
+    
+    
 }
 
